926339bdc1b823eb9499cea98e14b2dc026ec477
Arjun <arjunbalgovind@gmail.com>
2020-05-15 13:15:15 -0700

    Added assembly info for settings

--- src/core/Microsoft.PowerToys.Settings.UI.Runner/AssemblyInfo.cs
+++ src/core/Microsoft.PowerToys.Settings.UI.Runner/AssemblyInfo.cs
@@ -3,5 +3,18 @@
 // See the LICENSE file in the project root for more information.
 
 using System.Windows;
+using System.Reflection;
+using System.Runtime.InteropServices;
 
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+[assembly: AssemblyTitle("Settings")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft Corp.")]
+[assembly: AssemblyCopyright("Copyright (C) 2019 Microsoft Corp.")]
+[assembly: AssemblyProduct("Settings")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("0.18.0.0")]
+[assembly: AssemblyFileVersion("0.18.0.0")]
